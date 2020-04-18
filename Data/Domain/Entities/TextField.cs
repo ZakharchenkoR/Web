afbe801f77209ac114d711e8b1f2018f96f307fa
Roman Zakharchenko <superzaec22@gmail.com>
2020-04-18 12:03:06 +0300

    ServiceItem and BaseEntity

---
+++ Data/Domain/Entities/TextField.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Domain.Entities
+{
+    class TextField : BaseEntity
+    {
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Название страницы (заголовок)")]
+        public override string Title { get; set; } = "Информационная страница";
+
+        [Display(Name = "Содержание страницы")]
+        public override string Text { get; set; } = "Содержание заполняется администратором";
+    }
+}
