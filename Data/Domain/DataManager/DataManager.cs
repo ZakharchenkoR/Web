e40a5894d6d0df4dca80e2b9a3ce66b3735c5f10
Roman Zakharchenko <superzaec22@gmail.com>
2020-04-18 13:31:10 +0300

    DataManager

---
+++ Data/Domain/DataManager/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using Data.DataServices.IDataServices;
+
+namespace Data.Domain.DataManager
+{
+    public class DataManager
+    {
+        public ITextFieldRepository TextFieldServise { get; private set; }
+        public IServiceItemRepository ServiceItemService { get; private set; }
+
+        public DataManager(ITextFieldRepository textFieldRepository, IServiceItemRepository serviceItemRepository)
+        {
+            TextFieldServise = textFieldRepository;
+            ServiceItemService = serviceItemRepository;
+        }
+    }
+}
