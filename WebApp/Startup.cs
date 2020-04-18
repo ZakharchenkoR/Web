7294c122de4b28a26f71d06a6613c314333dbbe0
Roman Zakharchenko <superzaec22@gmail.com>
2020-04-18 13:50:17 +0300

    Startup

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,6 +1,12 @@
+using Data.DataServices.Base;
+using Data.DataServices.IDataServices;
+using Data.Domain;
+using Data.Domain.DataManager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +26,32 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             _configuration.Bind("Project", new Config());
+
+            services.AddTransient<ITextFieldRepository, TextFieldService>();
+            services.AddTransient<IServiceItemRepository, ServiceItemService>();
+            services.AddTransient<DataManager>();
+
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(x =>
+            {
+                x.User.RequireUniqueEmail = true;
+                x.Password.RequiredLength = 6;
+                x.Password.RequireNonAlphanumeric = false;
+                x.Password.RequireLowercase = false;
+                x.Password.RequireUppercase = false;
+                x.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(x =>
+            {
+                x.Cookie.Name = "myCompanyAuth";
+                x.Cookie.HttpOnly = true;
+                x.LoginPath = "/account/login";
+                x.AccessDeniedPath = "/account/accessdenied";
+                x.SlidingExpiration = true;
+            });
+
             services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
@@ -30,8 +62,12 @@ namespace WebApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
             app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
