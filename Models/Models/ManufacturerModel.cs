2b8531880b7c34f281d1e99e50bc21f9ec1f08a9
Roman Zakharchenko <superzaec22@gmail.com>
2020-05-03 17:07:15 +0300

    Models

---
+++ Models/Models/ManufacturerModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Models.Models
+{
+    public class ManufacturerModel : BaseModel
+    {
+        public Guid CountryId { get; set; }
+        public CountryModel CountryModel { get; set; }
+    }
+}
---
+++ Models/Models/PhoneModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models.Models
+{
+    public class PhoneModel : BaseModel
+    {
+        public Guid ManufacturerId { get; set; }
+        public ManufacturerModel ManufacturerModel { get; set; }
+        public decimal Prise { get; set; }
+        public int Count { get; set; }
+    }
+}
