c7d309bea5d455007024049148fc6cbf45ca4428
Roman Zakharchenko <superzaec22@gmail.com>
2020-05-03 17:15:17 +0300

    refactor

--- Services/DomainInterfaces/IManufacturerService.cs
+++ Services/DomainInterfaces/IManufacturerService.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Services.AppInterfaces
+namespace Services.DomainInterfaces
 {
     public interface IManufacturerService
     {
