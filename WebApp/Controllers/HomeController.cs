7da987918083e4bdd5f58429be78d497500dca2d
Roman Zakharchenko <superzaec22@gmail.com>
2020-05-03 14:32:23 +0300

    refactor

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,22 +1,12 @@
-﻿using System.Linq;
-using Data.Domain.DataContext;
-using Data.Domain.Entities;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly AppDbContext _appDbContext;
-
-        public HomeController(AppDbContext appDbContext)
-        {
-            _appDbContext = appDbContext;
-        }
         public IActionResult Index()
         {
-            var phones = _appDbContext.Phones.ToList();
-            return View(phones);
+            return View("Hello world");
         }
     }
 }
