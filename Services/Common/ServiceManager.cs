8a27a87858aa8f800bcc92c975461929632dd942
Roman Zakharchenko <superzaec22@gmail.com>
2020-05-03 17:56:26 +0300

    Common service

---
+++ Services/Common/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using Services.DomainInterfaces;
+
+namespace Services.Common
+{
+    public class ServiceManager : IServiceManager
+    {
+        public IManufacturerService ManufacturerService { get; }
+        public IPhoneService PhoneService { get; }
+        public ICountryService CountryService { get; }
+
+        public ServiceManager(IManufacturerService manufacturerService, IPhoneService phoneService, ICountryService countryService)
+        {
+            ManufacturerService = manufacturerService;
+            PhoneService = phoneService;
+            CountryService = countryService;
+        }
+    }
+}
