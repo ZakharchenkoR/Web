6a86512192bb5d01c2d821b04c01d04d59238b50
Roman Zakharchenko <superzaec22@gmail.com>
2020-05-03 17:13:12 +0300

    services

---
+++ Services/AppInterfaces/ICountryService.cs
@@ -0,0 +1,17 @@
+﻿using Data.Common;
+using Data.Domain.Entities;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services.AppInterfaces
+{
+    public interface ICountryService
+    {
+        Task<CountryModel> GetCountryAsync(Guid countryId);
+        Task<IList<CountryModel>> GetCountriesAsync(DataRequest<Country> request = null);
+        Task<int> UpdateCountryAsync(CountryModel model);
+        Task<int> DeleteCountryAsync(params CountryModel[] models);
+    }
+}
