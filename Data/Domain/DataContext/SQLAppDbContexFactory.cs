c1620123e0abe64314bd98444d750fbd14b69bb6
Roman Zakharchenko <superzaec22@gmail.com>
2020-05-03 13:19:32 +0300

    refactor

--- Data/Domain/DataContext/SQLAppDbContexFactory.cs
+++ Data/Domain/DataContext/SQLAppDbContexFactory.cs
@@ -8,7 +8,7 @@ namespace Data.Domain.DataContext
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionBilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionBilder.UseSqlServer("Data Source=localhost;Initial Catalog=PhoneShop;Integrated Security=SSPI");
+            optionBilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PhoneShop;Trusted_Connection=True;MultipleActiveResultSets=true");
             return new AppDbContext(optionBilder.Options);
         }
     }
